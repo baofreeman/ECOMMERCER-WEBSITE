84e0f3f77be28edb04edbbbe0d48b3b9ba150343
Hoang <freeman.dev10@gmail.com>
2024-08-29 16:15:21 +0700

    update get user

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,20 +1,35 @@
-import { useGetUserQuery } from "../api/endpoints/authApiSlice";
 import { useMemo } from "react";
+
+import { useGetUserQuery } from "../api/endpoints/authApiSlice";
+import Cookies from "js-cookie";
 
 const useAuth = () => {
   // Fetch user data
-  const { data: currentUser } = useGetUserQuery("currentUser");
+  const isRefreshToken = Cookies.get("refreshToken");
+  const {
+    data: currentUser,
+    isLoading,
+    isError,
+  } = useGetUserQuery("currentUser", {
+    skip: !isRefreshToken,
+  });
   console.log(currentUser);
-
   // Compute user object with default values and current user data
-  const user = useMemo(
-    () => ({
-      username: currentUser?.username || "",
-      userId: currentUser?._id,
-      roles: currentUser?.roles || ["user"],
-    }),
-    [currentUser]
-  );
+  const user = useMemo(() => {
+    if (!isRefreshToken || isLoading || isError || !currentUser) {
+      return {
+        username: "",
+        userId: null,
+        roles: ["user"], // default role if user is not logged in
+      };
+    }
+
+    return {
+      username: currentUser.username,
+      userId: currentUser._id,
+      roles: currentUser.roles || ["user"],
+    };
+  }, [isRefreshToken, currentUser, isLoading, isError]);
 
   return user;
 };
