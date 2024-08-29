14e5b250e6a8d3a16941ab21acb256863b4907cc
Hoang <freeman.dev10@gmail.com>
2024-08-29 10:33:54 +0700

    cloudinary

--- server/api/v1/middleware/cloudinaryMethod.js
+++ server/api/v1/middleware/cloudinaryMethod.js
@@ -1,6 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 
 const cloudinaryImageUploadMethod = async (file, folder) => {
+  const urlProtocol = process.env.CLOUDINARY_URL_PROTOCOL || "https";
+
   return new Promise((resolve) => {
     cloudinary.uploader.upload(file, { folder }, (error, result) => {
       if (error) {
@@ -8,8 +10,9 @@ const cloudinaryImageUploadMethod = async (file, folder) => {
           .status(401)
           .json({ success: false, message: error.message });
       }
+      const url = result.secure_url.replace(/^https:/, `${urlProtocol}:`);
       resolve({
-        url: result.secure_url,
+        url,
         id: result.public_id,
         folder: result.folder,
       });
