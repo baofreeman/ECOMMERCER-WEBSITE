456c2ae0a6090ecdbc1b16cc83d9931803363917
Hoang <freeman.dev10@gmail.com>
2024-08-31 00:51:32 +0700

    env

--- server/api/server.js
+++ server/api/server.js
@@ -2,13 +2,18 @@ const http = require("http");
 const app = require("./app");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5555;
+const NODE_ENV = process.env.NODE_ENV;
 
 const server = http.createServer(app);
 
 mongoose.connection.once("open", () => {
   console.log(`DataBase connecting`);
   server.listen(PORT, () => {
+<<<<<<< HEAD
     console.log(`Server running ${PORT}`);
+=======
+    console.log(`Server is running ${PORT}, NODE_ENV:${NODE_ENV}`);
+>>>>>>> a43ac0e (nodeenv)
   });
 });
 
