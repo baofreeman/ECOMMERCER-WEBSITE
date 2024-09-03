0d176841409b2ea8e2171e0e65fab4e2213afc4f
Hoang <freeman.dev10@gmail.com>
2024-09-03 21:17:09 +0700

    build

--- client/src/components/shared/FullScreenScroll.jsx
+++ client/src/components/shared/FullScreenScroll.jsx
@@ -8,14 +8,6 @@ const FullScreenScroll = ({ children }) => {
 
   const scrollToSection = (index) => {
     if (containerRef.current) {
-<<<<<<< HEAD
-      setIsScrolling(true);
-      containerRef.current.scrollTo({
-        top: index * window.innerHeight,
-        behavior: "smooth",
-      });
-      setTimeout(() => setIsScrolling(false), 300); // Stop scrolling for 300ms
-=======
       const totalSections = React.Children.count(children);
       const validIndex = Math.max(0, Math.min(index, totalSections - 1)); // Ensure index is within valid range
 
@@ -25,28 +17,10 @@ const FullScreenScroll = ({ children }) => {
         behavior: "smooth",
       });
       setTimeout(() => setIsScrolling(false), 800); // Stop scrolling for 300ms
->>>>>>> de15884 (public)
     }
   };
 
   const handleScroll = useCallback((e) => {
-<<<<<<< HEAD
-    e.preventDefault();
-    if (isScrolling) return; // Prevent scrolling while already scrolling
-
-    const now = performance.now(); // Get the current time
-    if (now - lastScrollTime < 300) return; // Limit scroll frequency to avoid excessive operations
-
-    const deltaY = e.deltaY;
-    const totalSections = React.Children.count(children);
-
-    if (deltaY > 0 && currentSection < totalSections - 1) {
-      setCurrentSection((prevSection) => prevSection + 1);
-    } else if (deltaY < 0 && currentSection > 0) {
-      setCurrentSection((prevSection) => prevSection - 1);
-    }
-
-=======
     if (isScrolling) return; // Prevent scrolling while already scrolling
     const now = performance.now(); // Get the current time
     if (now - lastScrollTime < 800) return; // Limit scroll frequency to avoid excessive operations
@@ -63,7 +37,6 @@ const FullScreenScroll = ({ children }) => {
       setCurrentSection((prevSection) => Math.max(prevSection - 1, 0));
     }
 
->>>>>>> de15884 (public)
     setLastScrollTime(now); // Update the last scroll time
   }, []);
 
@@ -82,19 +55,7 @@ const FullScreenScroll = ({ children }) => {
   }, [isScrolling, handleScroll]);
 
   return (
-<<<<<<< HEAD
-    <div
-      ref={containerRef}
-      style={{
-        height: "100dvh",
-        width: "100%",
-        overflow: "hidden",
-        scrollBehavior: "smooth",
-      }}
-    >
-=======
     <div className="public-scroll" ref={containerRef}>
->>>>>>> de15884 (public)
       {React.Children.map(children, (child) => (
         <div className="w-full h-full">{child}</div>
       ))}
