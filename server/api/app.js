014ace5df37c454e5a326acfaa31f0f38fbaf02f
Hoang <freeman.dev10@gmail.com>
2025-01-12 23:32:53 +0700

    update server

--- server/api/app.js
+++ server/api/app.js
@@ -65,4 +65,12 @@ app.use("/api/v1/user", user);
 app.use("/uploads", express.static("uploads"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
